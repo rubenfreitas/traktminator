1c4fd91fcb00a7a1a0e4ca3e5c646f290d53b31b
rfreitas <ruben.madtech@gmail.com>
2015-08-11 00:51:58 +0100

    first test with terminal-menu

--- traktminator.js
+++ traktminator.js
@@ -1,1 +1,25 @@
 #!/usr/bin/env node
+
+const tmenu = require('terminal-menu')
+    , path = require('path')
+
+var menu = tmenu({ width: 29, x: 4, y: 2 });
+
+menu.reset();
+
+menu.write('TRAKTMINATOR');
+menu.write('-------------------------\n');
+
+menu.add('TELL ME A STORY');
+
+menu.on('select', function (label) {
+    menu.close();
+    console.log('\nSELECTED: ' + label);
+});
+process.stdin.pipe(menu.createStream()).pipe(process.stdout);
+
+process.stdin.setRawMode(true);
+menu.on('close', function () {
+    process.stdin.setRawMode(false);
+    process.stdin.end();
+});
