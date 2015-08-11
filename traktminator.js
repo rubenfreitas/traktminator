d80eab340d576f75dc2699b0af46cdb5cda4b08e
rfreitas <ruben.madtech@gmail.com>
2015-08-11 01:25:05 +0100

    added some mock call to trakt apit

--- traktminator.js
+++ traktminator.js
@@ -2,8 +2,10 @@
 
 const tmenu = require('terminal-menu')
     , path = require('path')
+    , http = require('http')
+    , request = require('request')
 
-var menu = tmenu({ width: 29, x: 4, y: 2 });
+var menu = tmenu({ width: 29, x: 4, y: 2, bg: 17 });
 
 menu.reset();
 
@@ -15,6 +17,18 @@ menu.add('TELL ME A STORY');
 menu.on('select', function (label) {
     menu.close();
     console.log('\nSELECTED: ' + label);
+    request({
+      method: 'GET',
+      url: 'https://private-anon-d06ab65ca-trakt.apiary-mock.com/comments/417',
+      headers: {
+        'Content-Type': 'application/json',
+        'trakt-api-version': '2',
+        'trakt-api-key': '[client_id]'
+      }}, function (error, response, body) {
+      console.log('Status:', response.statusCode);
+      console.log('Headers:', JSON.stringify(response.headers));
+      console.log('Response:', body);
+    });
 });
 process.stdin.pipe(menu.createStream()).pipe(process.stdout);
 
